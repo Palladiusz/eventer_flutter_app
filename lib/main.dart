c7e895ba7faf031f6512f8839a55fbecf1e832cc
Palladiusz <palladiusz.ra@gmail.com>
2020-08-25 09:56:02 +0200

    removed neumpo and moor

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:eventer/screens/details_screen.dart';
 import 'package:eventer/screens/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,28 +9,12 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return NeumorphicApp(
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.dark,
-      theme: NeumorphicThemeData(
-        baseColor: Color(0xFFFFFFFF),
-        lightSource: LightSource.topLeft,
-        depth: -15,
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.lightBlue[800],
+        accentColor: Colors.cyan[600],
       ),
-      darkTheme: NeumorphicThemeData(
-          appBarTheme: NeumorphicAppBarThemeData(
-            iconTheme: IconThemeData(color: Colors.white),
-            buttonStyle: NeumorphicStyle(
-              depth: 1,
-              intensity: 0.9,
-              lightSource: LightSource.top,
-            ),
-          ),
-          baseColor: Colors.grey[800],
-          lightSource: LightSource.topLeft,
-          depth: -3,
-          intensity: 0.8,
-          defaultTextColor: Colors.yellowAccent),
       initialRoute: HomeScreen.id,
       routes: {
         HomeScreen.id: (context) => HomeScreen(),
--- lib/widgets/my_appbar.dart
+++ lib/widgets/my_appbar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
@@ -7,17 +6,19 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   const MyAppBar({this.title});
   @override
   Widget build(BuildContext context) {
-    return NeumorphicAppBar(
-      centerTitle: true,
-      title: Text(
-        title,
-        style: TextStyle(
-          fontSize: 24,
+    return Container(
+      child: AppBar(
+        centerTitle: true,
+        title: Text(
+          title,
+          style: TextStyle(
+            fontSize: 24,
+          ),
         ),
       ),
     );
   }
 
   @override
-  Size get preferredSize => Size.fromHeight(80);
+  Size get preferredSize => Size.fromHeight(60);
 }
